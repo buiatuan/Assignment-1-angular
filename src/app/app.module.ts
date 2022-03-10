746644e12eb35fda150dfe16dabf0a79b2556128
buiatuan <devils2tuan@gmail.com>
2022-03-10 19:25:44 +0700

    assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {SigninComponent} from "./signin/signin.component";
+import {RegisterComponent} from "./register/register.component";
+import {ForgotPasswordComponent} from "./forgotpassword/forgotpassword.component";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
+import {RouterModule, Routes} from "@angular/router";
+import {FormsModule} from "@angular/forms";
+import {CurrentWeatherComponent} from "./currentweather/currentweather.component";
+import {HttpClientModule} from "@angular/common/http";
+import {ForecastComponent} from "./forecast/forecast.component";
 
+const appRoutes: Routes =[
+  {path:"", component:SigninComponent},
+  {path:"forgotpassword", component:ForgotPasswordComponent},
+  {path:"regiter", component:RegisterComponent},
+  {path:"currentweather", component:CurrentWeatherComponent},
+  {path:"forecast", component:ForecastComponent},
+  ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    HeaderComponent,
+    FooterComponent,
+    CurrentWeatherComponent,ForecastComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/currentweather/currentweather.component.ts
@@ -0,0 +1,37 @@
+import {Component} from "@angular/core";
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+@Component({
+  selector: "current-weather",
+  templateUrl: "./currentweather.component.html"
+})
+export class CurrentWeatherComponent{
+  weather ={
+    city: "Hanoi, Vietnam",
+    temp: 15,
+    hump: 95,
+    press: 1005,
+    desc: "Trời rét, không sương mù, không mây",
+    icon: "04n"
+  }
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit(){
+    const url = "https://api.openweathermap.org/data/2.5/weather";
+      let parameters = new HttpParams();
+      parameters = parameters.append("q","Hanoi,vietnam");
+      parameters = parameters.append("appid","09a71427c59d38d6a34f89b47d75975c");
+      parameters = parameters.append("units","metric");
+    this.http.get(url,{
+      params:parameters
+    }).subscribe((rs:any)=>{
+      this.weather.city = rs.name+","+rs.sys.country;
+      this.weather.temp = rs.main.temp;
+      this.weather.hump = rs.main.humidity;
+      this.weather.press = rs.main.pressure;
+      this.weather.desc = rs.weather[0].description;
+      this.weather.icon = rs.weather[0].icon;
+    })
+    }
+
+}
